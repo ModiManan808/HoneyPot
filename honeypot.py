b70fdd1c166327cc59d20da72f713118146f4c33
Manan <160186382+ModiManan808@users.noreply.github.com>
2025-11-22 17:56:58 +0530

    Refactor honeypot.py to set defaults for username and password
    Set default username and password before running honeypot.

--- honeypot.py
+++ honeypot.py
@@ -1,10 +1,55 @@
-#Libraries
+# #Libraries
+# import argparse
+# from ssh_honeypot import *
+# from web_honeypot import *
+# #parse arguments
+
+# if __name__ == "__main__" :
+#     parser = argparse.ArgumentParser()
+
+#     parser.add_argument('-a','--address',type=str,required=True)
+#     parser.add_argument('-p','--port',type=int,required=True)
+#     parser.add_argument('-u','--username',type=str)
+#     parser.add_argument('-pw','--password',type=str)
+
+#     parser.add_argument('-s','--ssh',action='store_true')
+#     parser.add_argument('-w','--http',action="store_true")
+
+#     args = parser.parse_args()
+
+#     try :
+#         if args.ssh:
+#             print("[-] Running SSH honeypot...")
+#             honeypot(args.address,args.port,args.username,args.password)
+
+#             if not args.username :
+#                 username=None
+#             if not args.password:
+#                 password=None
+#         elif args.http :
+#             print("[-] Running HTTP WORDPRESS Honeypot")
+#             if not args.username :
+#                 args.username='admin'
+#             if not args.password:
+#                 args.password='password'
+
+#             print(f"Port:{args.port} Username :{args.username} Password:{args.password}")
+#             run_web_honeypot(args.port,args.username,args.password)
+
+#             pass
+#         else :
+#             print("[!]Choose a honeypot type (SSH --ssh) or  (HTTP --http).")
+#     except:
+#         print("EXITING HONYPOT")
+
+
+
+# Libraries
 import argparse
 from ssh_honeypot import *
 from web_honeypot import *
-#parse arguments
 
-if __name__ == "__main__" :
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument('-a','--address',type=str,required=True)
@@ -12,32 +57,28 @@ if __name__ == "__main__" :
     parser.add_argument('-u','--username',type=str)
     parser.add_argument('-pw','--password',type=str)
 
-    parser.add_argument('-s',action='store_true')
-    parser.add_argument('-w','--http',action="store_true")
+    parser.add_argument('-s','--ssh',action='store_true')
+    parser.add_argument('-w','--http',action='store_true')
 
     args = parser.parse_args()
 
-    try :
+    try:
+        # Set defaults BEFORE running honeypot
+        username = args.username or "admin"
+        password = args.password or "password"
+
         if args.ssh:
             print("[-] Running SSH honeypot...")
-            honeypot(args.address,args.port,args.username,args.password)
+            honeypot(args.address, args.port, username, password)
 
-            if not args.username :
-                username=None
-            if not args.password:
-                password=None
-        elif args.http :
-            print("[-] Running HTTP WORDPRESS Honeypot")
-            if not args.username :
-                args.username='admin'
-            if not args.password:
-                args.password='password'
+        elif args.http:
+            print("[-] Running HTTP WordPress Honeypot")
+            print(f"Port: {args.port} Username: {username} Password: {password}")
+            run_web_honeypot(args.port, username, password)
 
-            print(f"Port:{args.port} Username :{args.username} Password:{args.password}")
-            run_web_honeypot(args.port,args.username,args.password)
+        else:
+            print("[!] Choose a honeypot type (SSH --ssh) or (HTTP --http).")
 
-            pass
-        else :
-            print("[!]Choose a honeypot type (SSH --ssh) or  (HTTP --http).")
-    except:
-        print("EXITING HONYPOT")
+    except Exception as e:
+        print("EXITING HONEYPOT")
+        print("Error:", e)
